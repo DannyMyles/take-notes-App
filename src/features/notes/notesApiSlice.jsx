c91cf0cf2c77dd7e578ccb7c74f5b6a1bdcf9a4d
DannyMyles <dannymyles1999@gmail.com>
2024-01-03 11:01:01 +0300

    Implements user and note crud Ops

--- src/features/notes/notesApiSlice.jsx
+++ src/features/notes/notesApiSlice.jsx
@@ -1,7 +1,4 @@
-import {
-    createSelector,
-    createEntityAdapter
-} from "@reduxjs/toolkit";
+import { createSelector,createEntityAdapter } from "@reduxjs/toolkit";
 import { apiSlice } from "../../app/api/apiSlice"
 
 const notesAdapter = createEntityAdapter({
@@ -11,35 +8,61 @@ const notesAdapter = createEntityAdapter({
 const initialState = notesAdapter.getInitialState()
 
 export const notesApiSlice = apiSlice.injectEndpoints({
-    endpoints: builder => ({
-        getNotes: builder.query({
-            query: () => '/notes',
-            validateStatus: (response, result) => {
-                return response.status === 200 && !result.isError
-            },
-            keepUnusedDataFor: 5,
-            transformResponse: responseData => {
-                const loadedNotes = responseData.map(note => {
-                    note.id = note._id
-                    return note
-                });
-                return notesAdapter.setAll(initialState, loadedNotes)
-            },
-            providesTags: (result, error, arg) => {
-                if (result?.ids) {
-                    return [
-                        { type: 'Note', id: 'LIST' },
-                        ...result.ids.map(id => ({ type: 'Note', id }))
-                    ]
-                } else return [{ type: 'Note', id: 'LIST' }]
-            }
-        }),
+  endpoints: (builder) => ({
+    getNotes: builder.query({
+      query: () => "/notes",
+      validateStatus: (response, result) => {
+        return response.status === 200 && !result.isError;
+      },
+      keepUnusedDataFor: 5,
+      transformResponse: (responseData) => {
+        const loadedNotes = responseData.map((note) => {
+          note.id = note._id;
+          return note;
+        });
+        return notesAdapter.setAll(initialState, loadedNotes);
+      },
+      providesTags: (result, error, arg) => {
+        if (result?.ids) {
+          return [
+            { type: "Note", id: "LIST" },
+            ...result.ids.map((id) => ({ type: "Note", id })),
+          ];
+        } else return [{ type: "Note", id: "LIST" }];
+      },
     }),
-})
+    addNote: builder.mutation({
+      query: (initialNoteData) => ({
+        url: "/notes",
+        method: "POST",
+        body: {
+          ...initialNoteData,
+        },
+      }),
+      invalidatesTags: [{ type: "Note", id: "LIST" }],
+    }),
+    updateNote: builder.mutation({
+      query: (initialNoteData) => ({
+        url: "/notes",
+        method: "PATCH",
+        body: {
+          ...initialNoteData,
+        },
+      }),
+      invalidatesTags: (result, err, arg) => [{ type: "Note", id: arg.id }],
+    }),
+    deleteNote: builder.mutation({
+      query: ({ id }) => ({
+        url: "/notes",
+        method: "DELETE",
+        body: { id },
+      }),
+      invalidatesTags: (result, err, arg) => [{ type: "Note", id: arg.id }],
+    }),
+  }),
+});
 
-export const {
-    useGetNotesQuery,
-} = notesApiSlice
+export const { useGetNotesQuery, useAddNoteMutation, useUpdateNoteMutation, useDeleteNoteMutation} = notesApiSlice
 
 // returns the query result object
 export const selectNotesResult = notesApiSlice.endpoints.getNotes.select()
